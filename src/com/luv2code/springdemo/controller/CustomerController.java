4bfeea1fa2a91552a8deddf5454d08b03aeb583b
Matheus Melo <matheuslmundim@gmail.com>
2023-01-24 10:39:22 -0300

    Changing @RequestMapping to @GetMapping, delegating transaction management to Service Layer instead of DAO

--- src/com/luv2code/springdemo/controller/CustomerController.java
+++ src/com/luv2code/springdemo/controller/CustomerController.java
@@ -5,22 +5,21 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.luv2code.springdemo.dao.CustomerDAO;
 import com.luv2code.springdemo.entity.Customer;
+import com.luv2code.springdemo.service.CustomerService;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
 	@Autowired
-	private CustomerDAO customerDAO;
+	private CustomerService customerService;
 	
-	@RequestMapping("/list")
+	@GetMapping("/list")
 	public String listCustomers(Model model) {
-		List<Customer> customers = customerDAO.getCustomers();
-//		System.out.println(customers);
+		List<Customer> customers = customerService.getCustomers();
 		model.addAttribute("customers",customers);
 		return "list-customers";
 	}
