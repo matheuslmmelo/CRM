01ee06f08bc91ba6e2f9b101afa1a94ff96bb2f0
Matheus Melo <matheuslmundim@gmail.com>
2023-01-24 09:58:17 -0300

    Correcting entity mapping attributes in Customer.java, getting customers list, adding css and welcome redirect page (index)

--- src/com/luv2code/springdemo/controller/CustomerController.java
+++ src/com/luv2code/springdemo/controller/CustomerController.java
@@ -1,15 +1,29 @@
 package com.luv2code.springdemo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.luv2code.springdemo.dao.CustomerDAO;
+import com.luv2code.springdemo.entity.Customer;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
-	@RequestMapping("list")
+	@Autowired
+	private CustomerDAO customerDAO;
+	
+	@RequestMapping("/list")
 	public String listCustomers(Model model) {
+		List<Customer> customers = customerDAO.getCustomers();
+//		System.out.println(customers);
+		model.addAttribute("customers",customers);
 		return "list-customers";
 	}
+	
+	
 }
--- src/com/luv2code/springdemo/dao/CustomerDAOImpl.java
+++ src/com/luv2code/springdemo/dao/CustomerDAOImpl.java
@@ -8,9 +8,11 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import com.luv2code.springdemo.entity.Customer;
 
+@Repository
 public class CustomerDAOImpl implements CustomerDAO {
 
 	@Autowired
